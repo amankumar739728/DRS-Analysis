2973706066bbd61349816443aecb9133012c82dc
amankumar739728 <aman.kumar739728@gmail.com>
2023-10-10 13:04:34 +0530

    first commit

---
+++ run.sh
@@ -0,0 +1,9 @@
+ # create a folder in (c:\) local repository and run the below command to execute the code output in vscode
+
+1. C:\DRS-Python-Project>git clone https://github.com/AnkitM18-tech/Third-Umpire-DRS-System.git
+
+2. C:\DRS-Python-Project>cd Third-Umpire-DRS-System
+
+3. C:\DRS-Python-Project\Third-Umpire-DRS-System>pip install -r requirements.txt
+
+4. C:\DRS-Python-Project\Third-Umpire-DRS-System>python drs.py  
